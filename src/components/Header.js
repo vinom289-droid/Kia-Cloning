ad1e5f646338c74fb2f827c9b1503664cbdd1685
vinom289-droid <vinom289@gmail.com>
2026-06-09 12:34:25 +0530

    Kia-2

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,6 @@
 import pin from '../assest/pin-1.png'
 import logo from '../assest/kia logo.png'
 import search from '../assest/search.png'
-import { useNavigate } from 'react-router-dom'
-
 function Header() {
     return (
         <header className="parent">
